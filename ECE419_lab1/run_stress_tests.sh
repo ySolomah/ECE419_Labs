72227ad37a86f80fc7915b23b94715de71579f14
Pavone  Rohan <pavonero@ug154.eecg.toronto.edu>
2018-01-25 01:44:31 -0500

    Add stress test running script with options

---
+++ ECE419_lab1/run_stress_tests.sh
@@ -0,0 +1,30 @@
+# variables are:
+# cached/not cached
+# concurrency
+# put ratio
+# total requests - will keep constant at 10000
+
+ratio=(0.99 0.9 0.8 0.7 0.6 0.5 0.4 0.3 0.2 0.1 0.01)
+total_requests=10000
+concurrency=(1 10 20 30 40 50 100)
+cache=(0 10)
+cache_scheme=("FIFO" "LFU" "LRU")
+server="localhost"
+port=50000
+
+for cache_method in "${cache_scheme[@]}"
+do
+    for cache_size in "${cache[@]}"
+    do
+        # start up the server
+        java -jar m1-server.jar $port $cache_size $cache_scheme &
+        for r in "${ratio[@]}"
+        do
+            for c in "${concurrency[@]}"
+            do
+                java -jar m1-stress.jar $server $port $total_requests $c $r > stress_results/stress_test_${r}_${c}_${cache_size}_${cache_method}.txt
+            done
+        done
+    done
+done
+
